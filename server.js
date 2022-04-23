cfca74a2d1b98fcd5c3904087eb633d473deda76
Jay Thakkar <jaythakkar1151@gmail.com>
2022-04-23 11:53:31 +0530

    initiat setup

---
+++ server.js
@@ -0,0 +1,18 @@
+'use strict'
+
+require('dotenv').config()
+const Express = require('express')
+const bodyParser = require('body-parser')
+
+const app = Express()
+
+app.use(bodyParser.json())
+
+const IndexRoute = require('./routes/index')
+
+app.use('/',IndexRoute)
+
+app.listen(process.env.APP_PORT,()=>{
+    console.log(`Your app is running on port number ${process.env.PORT}`);
+});
+
